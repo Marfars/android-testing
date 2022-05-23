6593ba83f175c828d18d5cf946661c8913e8e55b
李宇茜 <736866102@qq.com>
2022-05-23 21:57:55 +0800

    feature: first test

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
@@ -3,6 +3,7 @@ package com.example.android.architecture.blueprints.todoapp.statistics
 import com.example.android.architecture.blueprints.todoapp.data.Task
 import org.junit.Assert.*
 import org.junit.Test
+import org.hamcrest.CoreMatchers.`is`
 
 class StatisticsUtilsTest {
 
@@ -10,7 +11,7 @@ class StatisticsUtilsTest {
     fun getActiveAndCompletedStats_noCompleted_returnsHundredZero() {
         val tasks = listOf(Task("title", "desc",false))
         val result = getActiveAndCompletedStats(tasks)
-//        assertEquals(result.completedTasksPercent, 0.0.toFloat())
-        assertEquals(result.activeTasksPercent, 100.0.toFloat())
+        assertThat(result.completedTasksPercent, `is`(0f))
+        assertThat(result.activeTasksPercent, `is`(100f))
     }
 }
