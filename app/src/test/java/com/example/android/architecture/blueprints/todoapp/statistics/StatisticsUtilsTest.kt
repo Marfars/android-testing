d5cc95b40d3d6871305fa51bf76be55fd1bf7135
李宇茜 <736866102@qq.com>
2022-05-23 21:49:43 +0800

    fix: build failed

---
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
@@ -0,0 +1,16 @@
+package com.example.android.architecture.blueprints.todoapp.statistics
+
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import org.junit.Assert.*
+import org.junit.Test
+
+class StatisticsUtilsTest {
+
+    @Test
+    fun getActiveAndCompletedStats_noCompleted_returnsHundredZero() {
+        val tasks = listOf(Task("title", "desc",false))
+        val result = getActiveAndCompletedStats(tasks)
+//        assertEquals(result.completedTasksPercent, 0.0.toFloat())
+        assertEquals(result.activeTasksPercent, 100.0.toFloat())
+    }
+}
