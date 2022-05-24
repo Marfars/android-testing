d603946e2e1b9009cabb6f118517d040a1891e21
李宇茜 <736866102@qq.com>
2022-05-24 22:00:31 +0800

    feature: add my own test

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -1,19 +1,16 @@
 package com.example.android.architecture.blueprints.todoapp.tasks
 
-import android.app.usage.UsageEvents
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.lifecycle.Observer
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.example.android.architecture.blueprints.todoapp.Event
+import com.example.android.architecture.blueprints.todoapp.R
 import com.example.android.architecture.blueprints.todoapp.getOrAwaitValue
 import org.hamcrest.Matchers.*
 import org.junit.Assert
-import org.junit.Assert.*
+import org.junit.Assert.assertThat
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import java.lang.Exception
 
 @RunWith(AndroidJUnit4::class)
 class TasksViewModelTest {
@@ -27,6 +24,20 @@ class TasksViewModelTest {
         viewModel.addNewTask()
         val value = viewModel.newTaskEvent.getOrAwaitValue()
         assertThat(value.getContentIfNotHandled(), (not(nullValue())))
+    }
+
+    @Test
+    fun setFilterAllTasks_tasksAddViewVisible() {
+
+        // Given a fresh ViewModel
+        val viewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
+        // When the filter type is ALL_TASKS
+        viewModel.setFiltering(TasksFilterType.ALL_TASKS)
+        // Then the "Add task" action is visible
+        val value = viewModel.tasksAddViewVisible.getOrAwaitValue()
+        assertThat(value, `is`(true))
+        val currentLabel = viewModel.currentFilteringLabel.getOrAwaitValue()
+        assertThat(currentLabel, `is`(R.string.label_all))
     }
 
 }
