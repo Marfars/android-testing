5a44f1c450bd48191371b70722cfdb3278b48f36
xixi <736866102@qq.com>
2022-05-24 19:30:11 +0800

    build: add core test dependency

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -1,27 +1,28 @@
 package com.example.android.architecture.blueprints.todoapp.tasks
 
 import android.app.usage.UsageEvents
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.android.architecture.blueprints.todoapp.Event
 import org.hamcrest.Matchers.`is`
 import org.junit.Assert
 import org.junit.Assert.*
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class TasksViewModelTest {
 
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
     @Test
     fun addNewTask_setsNewTaskEvent() {
         val viewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
 
         viewModel.addNewTask()
-        assertTrue(1==1)
-/*
-        val event = viewModel.newTaskEvent.value
-        assertThat(event, `is`(Event(Unit)))*/
     }
 
 }
